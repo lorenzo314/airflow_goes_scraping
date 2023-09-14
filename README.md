38c8f7cc74cb34b0a1f0eae5a591403fe53d39e9
Lorenzo Faccioli <lorenzo.faccioli@octopeek.com>
2023-09-14 12:15:18 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Scrape ACE data
---
+++ utils/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='goes_utils_raw',
+      version='1.0',
+      py_modules=['goes_utils_raw'],
+)
+
+setup(name='goes_utils_silver',
+      version='1.0',
+      py_modules=['goes_utils_silver'],
+)
